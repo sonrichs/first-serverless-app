729651050f0371ad3e4c8d5d7739481c2997b2aa
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-03-26 23:31:00 -0300

    add config file

---
+++ src/common/config/config.ts
@@ -0,0 +1,4 @@
+export const {
+    example_env: test_example_env = 'default',
+    test_api_key = 'default'
+} = process.env
--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { test_example_env } from '../src/common/config/config'
 
 export const example = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    const myEnvVariable = test_example_env
+    
     return {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json'
         },
-        body: "Funciono mirame :)"
+        body: JSON.stringify({
+            message: `${myEnvVariable} Funciono mirame :)`
+        })
     }
 }
